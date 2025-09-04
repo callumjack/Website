046c3dba9b5b21247c61b2bbad01c85f960fd6fd
jackwrfuller <jack.fuller101@gmail.com>
2025-09-04 12:57:33 +1000

    udpate

--- lagoon/entrypoints/10-environment.sh
+++ lagoon/entrypoints/10-environment.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-echo " ==> Setting Umami database URL"
-
-# Check required environment variables
-for var in DB_USERNAME DB_PASSWORD DB_HOST DB_PORT DB_DATABASE; do
-    if [ -z "$(eval echo \$$var)" ]; then
-        echo "Error: Required environment variable $var is not set"
-        exit 1
-    fi
-done
-
-export DATABASE_URL="mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
-echo "OK!"
+#echo " ==> Setting Umami database URL"
+#
+## Check required environment variables
+#for var in DB_USERNAME DB_PASSWORD DB_HOST DB_PORT DB_DATABASE; do
+#    if [ -z "$(eval echo \$$var)" ]; then
+#        echo "Error: Required environment variable $var is not set"
+#        exit 1
+#    fi
+#done
+#
+#export DATABASE_URL="mysql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}"
+#echo "OK!"
 
 echo " ==> Setting HASH_SALT"
 if [ -z "${HASH_SALT}" ]; then
